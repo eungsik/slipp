80241e9ea60bf853b4d7d7e0d5c3a93fa680d599
eungsik <eungsik80@nate.com>
2017-06-19 11:25:58 +0900

    컨트롤러와 DAO연동

---
+++ qna/src/main/java/net/slipp/web/users/UserController.java
@@ -0,0 +1,38 @@
+package net.slipp.web.users;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import net.slipp.dao.users.UserDao;
+import net.slipp.domain.users.User;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+		
+	@Autowired
+	private UserDao userDao;
+	
+	@RequestMapping("/form")
+	public String form() {		
+		return "users/form";
+	}
+	
+	@RequestMapping(value="", method=RequestMethod.POST)
+	public String create(User user) {
+		logger.debug("User : {}", user);
+		userDao.create(user);
+		logger.debug("Database : {}", userDao.findById(user.getUserId()));
+		
+		return "users/form";
+	}
+}
